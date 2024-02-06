02d0311867772f07a290cc01f771f4c6e9ce0a51
Emmanuel <96310496+eulasi@users.noreply.github.com>
2024-02-06 07:21:28 -0500

    Added Not Null assertion tests. Test Coverage Approach

---
+++ app/src/test/java/com/example/navbaranalysisone/data/model/EndPtTwoModelTest.kt
@@ -0,0 +1,14 @@
+package com.example.navbaranalysisone.data.model
+
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class EndPtTwoModelTest {
+
+    @Test
+    fun endPtTwoModel_isNotNull() {
+        val endPtTwoModelItem = EndPtTwoModelItem()
+        assertNotNull(endPtTwoModelItem)
+    }
+
+}
